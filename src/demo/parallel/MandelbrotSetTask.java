04f92a6ea2e5e19f8ad85a75e9bb223ac467d597
Nadya Gornostay 400541 <chamamilla@tut.by>
2017-04-28 15:03:13 +0300

    fractal equation change

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,8 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            //c = c.times(c).plus(comp);
+            c = c.times(c).times(c).plus(comp);// fc(z)=z^4+c
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
@@ -353,17 +354,17 @@ class MandelbrotSetTask extends Task<Long> {
         Color[] cc = {
             //Color.rgb(40, 0, 0),
             //Color.RED,
-            Color.rgb(90, 0, 100),
-            Color.DARKVIOLET,
+            Color.rgb(0, 100, 100),
+            Color.YELLOW,
             Color.WHITE,
             //Color.RED,
-            Color.DARKVIOLET,
+            Color.BLUE,
             //Color.rgb(100, 0, 0),
-            Color.rgb(50, 0, 100),
+            Color.rgb(0, 0, 100),
             //Color.RED,
             //Color.rgb(50, 0, 0)
-            Color.DARKVIOLET,
-            Color.rgb(50, 0, 100),
+            Color.RED,
+            Color.rgb(20, 0, 0),
         };
         
         /**
